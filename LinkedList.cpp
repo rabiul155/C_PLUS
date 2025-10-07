55960bb250ddaab9e895d8e36cace1f455e9b783
rabiul155 <siyamict@gmail.com>
2025-10-07 11:56:42 +0600

    reverse k node

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,52 +1,52 @@
 #include<bits/stdc++.h>
 using namespace std;
 class node {
-  public : 
+public:
   int data;
   node* next;
-  node(int val){
+  node(int val) {
     data = val;
     next = NULL;
   }
 };
 
-void insetAtHead (node* &head, int val){
+void insetAtHead(node*& head, int val) {
   node* n = new node(val);
   n->next = head;
   head = n;
 }
 
-void insertAtTail (node* &head , int val){
+void insertAtTail(node*& head, int val) {
   node* n = new node(val); // here n is the address of new node. node created in heap and its address stored in stack.
 
-  if(head == NULL){
+  if (head == NULL) {
     head = n;
     return;
   }
   node* temp = head;
 
-  while (temp->next != NULL){
-    temp = temp->next;   
+  while (temp->next != NULL) {
+    temp = temp->next;
     // here temp is address. we know if we want to get value from address we need dereference so basically it is *temp is the object created in heap after that we access val or next like this (*temp).val or (*temp).next....sorthand (*temp).next == temp->next.....(*temp).val == temp->val
   }
   temp->next = n;
 }
 
-void display(node* head){
+void display(node* head) {
   node* temp = head;
 
-  while (temp!= NULL){
-      cout << temp->data << " ";
-      temp = temp->next;
+  while (temp != NULL) {
+    cout << temp->data << " ";
+    temp = temp->next;
   }
-  cout << endl; 
+  cout << endl;
 }
 
-bool findValue (node* head , int val){
+bool findValue(node* head, int val) {
   node* temp = head;
 
-  while(temp != NULL){
-    if(temp->data == val){
+  while (temp != NULL) {
+    if (temp->data == val) {
       return true;
     }
     temp = temp->next;
@@ -54,45 +54,45 @@ bool findValue (node* head , int val){
   return false;
 }
 
-void deleteAtHead (node* &head){
+void deleteAtHead(node*& head) {
   node* deleteNode = head;
   head = head->next;
   delete deleteNode;
 }
 
-void deleteNode (node* &head , int val){
+void deleteNode(node*& head, int val) {
 
 
   // if list is empty
-  if(head == NULL){
+  if (head == NULL) {
     return;
   }
 
   // if only one node
-  if(head->next == NULL){
+  if (head->next == NULL) {
     deleteAtHead(head);
   }
 
 
   node* temp = head;
 
-  while(temp->next->data != val){
+  while (temp->next->data != val) {
     temp = temp->next;
   }
-  node * todelete = temp->next;
+  node* todelete = temp->next;
   temp->next = temp->next->next;
   delete todelete;
 
 }
 
-node* reverseList (node* head){
+node* reverseList(node* head) {
 
   node* prev = NULL;
   node* curr = head;
   node* next;
 
 
-  while(curr != NULL){
+  while (curr != NULL) {
     next = curr->next;
     curr->next = prev;
     prev = curr;
@@ -102,14 +102,36 @@ node* reverseList (node* head){
   return prev;
 }
 
+node* reverseKNode(node* head, int k) {
 
-int main (){
+  node* prev = NULL;
+  node* curr = head;
+  node* next;
+
+  int count = 0;
+  while (curr != NULL && count < k) {
+    next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+    count++;
+  }
+
+  if (next != NULL) {
+    head->next = reverseKNode(next, k);
+  }
+
+  return prev;
+}
+
+int main() {
   node* head = NULL;
-  insertAtTail(head,1);
-  insertAtTail(head,2);
-  insertAtTail(head,3);
-  insertAtTail(head,4);
-  insertAtTail(head,5);
+  insertAtTail(head, 1);
+  insertAtTail(head, 2);
+  insertAtTail(head, 3);
+  insertAtTail(head, 4);
+  insertAtTail(head, 5);
+
 
   // display(head);
   // insetAtHead(head,0);
@@ -124,8 +146,11 @@ int main (){
 
   display(head);
 
-  cout << "Display after reverse\n";
-  node* newHead = reverseList(head);
+  // cout << "Display after reverse\n";
+  // node* newHead = reverseList(head);
+  // display(newHead);
+
+  node* newHead = reverseKNode(head, 3);
   display(newHead);
 
   return 0;
