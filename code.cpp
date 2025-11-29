95641e50e3417657607b9a06993a3c72e0b6f655
rabiul155 <siyamict@gmail.com>
2025-11-29 13:20:58 +0600

    update

--- code.cpp
+++ code.cpp
@@ -1,18 +1,21 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
 
-void solve(){
 
-
+void solve() {
+    
 }
 
 int main() {
 	// author : siyam
-	int t; cin >> t;
+	ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+	ll t; cin >> t;
 	while(t--){
 	  solve();
 	}
 	return 0;
 }
-
 // g++ code.cpp && ./a.exe
